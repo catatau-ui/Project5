b6201366dce5eacbf9d44bbe599c084b9e12bfb7
Caty <emaildacatarinafaria13@gmail.com>
2026-04-13 11:00:21 +0100

    Adicionar arquivos de projeto.

---
+++ exercicios.1.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int cateto(int a, int b)
+{
+	return sqrt(a * a * b * b);
+}
+
+int perguntar()
+{
+	int a, int b;
+	cout << "Digite o valor dos dois catetos menores de um triangulo: ";
+	cin >> a >> b;
+	return cateto(a, b);
+}
+int main()
+{
+	int perguntar();
+	int cateto();
+	cout << "A resposta é: " << cateto() << endl;
+	return 0;
+}
