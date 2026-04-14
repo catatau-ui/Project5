e770a47a5b32c1258996e61d1c209aaa40a00542
Caty <emaildacatarinafaria13@gmail.com>
2026-04-14 10:49:03 +0100

    mini correcao.

--- exercicios.1.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-
-int main()
-{
-	// Imprime cabeçalho azul usando sequência ANSI (funciona na maioria dos terminais)
-	cout << "\x1b[34m" << "---------Mini_game---------" << "\x1b[0m" << endl;
-	return 0;
-}
